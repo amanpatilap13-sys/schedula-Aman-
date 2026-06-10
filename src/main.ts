7233b8ccca4fe7ddb33b3f8f78f7e68c5f56dcd6
Aman Patil <Amanpatilap13@gmail.com>
2026-06-10 12:13:36 +0530

    NestJS project setup completed

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-<<<<<<< HEAD
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }),
-);
-=======
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
->>>>>>> c2b0c1e (Implement role based authentication system)
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
